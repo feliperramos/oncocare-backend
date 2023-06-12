80dca5befe4ee0af0906de1e7d47d3ce9fce5f8d
feliperramos <feliperramos@icloud.com>
2023-06-11 23:38:04 -0300

    forgot password logics

--- src/routes/reset-password.ts
+++ src/routes/reset-password.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from "express";
-
-import mailer from "../config/mailer";
+import bcrypt from "bcrypt";
 
 import { User } from "../models/users";
+import { GetToken } from "../middleware/jwt";
+import { Authentication } from "../middleware/auth";
 
 const router = express.Router();
 
@@ -13,8 +14,6 @@ router.post("/validate-token", [], async (req: Request, res: Response) => {
     const user = await User.findOne({ email })
       .select("+passwordResetToken")
       .select("+passwordResetExpires");
-
-    console.log(user);
 
     if (token !== user?.passwordResetToken) {
       res.status(404).json({
@@ -32,11 +31,43 @@ router.post("/validate-token", [], async (req: Request, res: Response) => {
         errorMessage:
           "Token is already expired, please try to send a new reset code.",
       });
+
+    res.status(200).json({
+      status: true,
+      message: "Token validated!!",
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      authToken: GetToken(user!),
+    });
   } catch (err) {
     res.status(400).json({ error: "Error on validate Token. Try Again!" });
   }
 });
 
-router.put("/reset-password", [], async (req: Request, res: Response) => {});
+router
+  .use(Authentication)
+  .put("/reset-password", [], async (req: Request, res: Response) => {
+    const { email, newPassword } = req.body;
+
+    try {
+      const user = await User.findOne({ email });
+
+      if (!user) res.status(401).json({ message: "User not found!" });
+
+      const now = new Date();
+
+      const encryptPassword = await bcrypt.hash(newPassword, 10);
+
+      await User.findByIdAndUpdate(user?.id, {
+        $set: {
+          password: encryptPassword,
+          updated_at: now,
+        },
+      });
+
+      res.status(200).json({ message: "password reset!" });
+    } catch (err) {
+      res.status(400).json({ error: "Error on reset password. Try Again!" });
+    }
+  });
 
 export { router as ResetPasswordRouter };
